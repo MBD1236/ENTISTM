9879d2d509955a09e61e9a331bd1f67aa9289595
MBD1236 <bobobonkon25@gmail.com>
2024-05-06 22:15:07 +0000

    Mise en place de l'import des notes j-2

--- app/Livewire/Departements/GenieInformatique/EnregistrementNote.php
+++ app/Livewire/Departements/GenieInformatique/EnregistrementNote.php
@@ -2,14 +2,53 @@
 
 namespace App\Livewire\Departements\GenieInformatique;
 
+use App\Models\Matiere;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class EnregistrementNote extends Component
 {
+    use WithFileUploads;
+
+    public $fichier;
+    public $matiere_id;
+
+
+     /**
+     * rules: Règles de validation du fichier à importer
+     *
+     * @var array
+     */
+    protected $rules = [
+        'fichier' => ['required'],
+        'matiere_id' => ['required']
+    ];
+
+    // public function importer() {
+    //     $this->validate();
+    //     try {
+    //         if($this->fichier) {
+    //             Excel::import(new NoteImport($this->matiere_id), $this->fichier);
+    //             $this->reset('fichier');
+    //             session()->flash('success', 'Import effectué avec succès!');
+
+    //         }
+    //     } catch (\Throwable $th) {
+    //         session()->flash('danger', "Echec de l'import !");
+    //     }
+
+    // }
+
+    public function clearStatus()
+    {
+        $this->resetErrorBag();
+    }
     #[Layout("components.layouts.template-departements")]
     public function render()
     {
-        return view('livewire.departements.genie-informatique.enregistrement-note');
+        return view('livewire.departements.genie-informatique.enregistrement-note',[
+            "matieres"=> Matiere::all(),
+        ]);
     }
 }
--- resources/views/livewire/departements/genie-informatique/enregistrement-note.blade.php
+++ resources/views/livewire/departements/genie-informatique/enregistrement-note.blade.php
@@ -1,7 +1,52 @@
 <div>
+    
+    @if(session('success'))
+    <div class="alert alert-success">
+        {{session('success')}}
+    </div>
+    @endif
+    @if(session('danger'))
+    <div class="alert alert-danger">
+        {{session('danger')}}
+    </div>
+    @endif
+
+    @if($errors->any())
+        <ul>
+        @foreach ($errors->all() as $item)
+            <li>{{ $item }}</li>
+        @endforeach
+        </ul>
+    @endif
+
     <div class="card">
         <div class="card-header card-head">
             <h1 class="bg-card text-center text-white card-head"><i class="bi bi-journal-richtext me-3"></i>Enrégistrement des notes</h1>
         </div>
+        <form action="" wire:submit.prevent='importer' enctype="multipart/form-data">
+            <div class="row">
+                <div class="col-md-4">
+                    <input type="file" wire:model='fichier' class="form-control @error('fichier') is-invalid @enderror" wire:focus.live.debounce.1ms='clearStatus'>
+                    <div class="invalid-feedback">@error('fichier') {{ $message }} @enderror</div>
+
+                </div>
+
+                <div class="col-md-4">
+                    <select wire:model="matiere_id" id="matiere_id" class="form-select @error('matiere_id') is-invalid @enderror">
+                        <option value="0">Selectionner la matiere</option>
+                        @foreach ($matieres as $matiere)
+                            <option value="{{ $matiere->id }}" wire:key="{{ $matiere->id }}">{{ $matiere->matiere }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="col-md-4">
+                    <button type="submit" class="btn-modal"><i class="fa fa-file me-2 mt-2"></i>Importer</button>
+                </div>
+            </div>
+         
+            
+        </form>
     </div>
+
 </div>
